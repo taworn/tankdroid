c76b6c983d3977f4a15690e2f0da66d3150edb8b
Taworn T <taworn@gmail.com>
2016-09-26 17:10:35 +0700

    - Set project name to diy.tankdroid.

---
+++ app/src/main/java/diy/tankdroid/MainActivity.java
@@ -0,0 +1,14 @@
+package diy.tankdroid;
+
+import android.os.Bundle;
+
+import org.libsdl.app.SDLActivity;
+
+public class MainActivity extends SDLActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+}
